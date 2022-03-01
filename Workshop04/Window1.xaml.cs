c8d4b7661587f24deda44584b20de5aa89173a8a
meszaroszsolt633 <meszaroszsolt633@gmail.com>
2022-03-01 15:11:57 +0100

    ..

--- Workshop04/Window1.xaml.cs
+++ Workshop04/Window1.xaml.cs
@@ -25,7 +25,6 @@ namespace Workshop04
         }
         private void NewHeroButton(object sender, RoutedEventArgs e)
         {
-            (this.DataContext as MainWindowsViewModel).AddToArmy();
         }
     }
 }
